c8242c778e123a52994c049373be3d619211341c
Jnojokes <francesco.testatonda@studio.unibo.it>
2023-06-12 18:34:47 +0200

    NP

--- squealer-app/src/pages/NewPostPage.jsx
+++ squealer-app/src/pages/NewPostPage.jsx
@@ -5,6 +5,9 @@ import axios from "axios";
 function NewPostPage() {
   const [avatarPath, setAvatarPath] = useState("https://placekitten.com/100/100");
   const [postContent, setPostContent] = useState("");
+  const [letterCount, setLetterCount] = useState(0);
+  const [imagePath, setImagePath] = useState("");
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const userID = localStorage.getItem("userID")?.toString();
 
@@ -25,42 +28,86 @@ function NewPostPage() {
 
   function handlePublishClick() {
     const postData = {
-      sender: userID, // Sostituisci userID con l'ID corretto del mittente del post
+      sender: localStorage.getItem("userID"),
       recipients: [],
       text: postContent,
       timestamp: new Date(),
-      image_path: "",
+      image_path: imagePath,
       geolocation: { lat: 0, lon: 0 },
       reactions: { positive: 0, negative: 0 },
     };
 
-    axios.post("/api/posts", postData) // Sostituisci "/api/posts" con l'URL corretto per la tua API dei post
-      .then(response => {
-        console.log(response.data); // Puoi gestire la risposta del server qui
+    axios
+      .post("http://localhost:3000/post", postData)
+      .then((response) => {
+        console.log(response.data);
       })
-      .catch(error => {
-        console.error(error); // Puoi gestire l'errore qui
+      .catch((error) => {
+        console.error(error);
       });
+  }
+
+  useEffect(() => {
+    let count = postContent.replace(/\s/g, "").length;
+    if (imageLoaded) {
+      count += 120;
+    }
+    setLetterCount(count);
+  }, [postContent, imageLoaded]);
+
+  function handleImageUpload(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePath(reader.result);
+        setImageLoaded(true);
+      };
+      reader.readAsDataURL(file);
+    }
   }
 
   return (
     <div className="flex justify-center">
-      <div className="max-w-md w-full bg-white shadow-md rounded-md p-4">
+      <div className="max-w-xl w-full bg-white shadow-md rounded-md p-4">
+        <h1 className="text-2xl font-bold mb-4 text-center">Scrivi un nuovo Squeal!</h1>
         <div className="flex items-center mb-4">
-          <img
-            className="w-10 h-10 rounded-full mr-3"
-            src={avatarPath}
-            alt="User Avatar"
-          />
-          <div className="flex-1">
-            <input
-              className="border-b border-gray-300 outline-none w-full"
-              type="text"
+          <img className="w-10 h-10 rounded-full mr-3" src={avatarPath} alt="User Avatar" />
+          <div className="flex-1 relative">
+            {imageLoaded && (
+              <img
+                className="w-full rounded-md mb-2"
+                src={imagePath}
+                alt="Uploaded Image"
+              />
+            )}
+            <textarea
+              className="border border-gray-300 outline-none w-full p-2"
               placeholder="Scrivi un nuovo..."
               value={postContent}
               onChange={(e) => setPostContent(e.target.value)}
+            ></textarea>
+          </div>
+        </div>
+        <div className="text-right mb-2">
+          <span>Conteggio lettere: {letterCount}</span>
+        </div>
+        <div className="flex justify-between items-center">
+          <div>
+            <label htmlFor="image-upload" className="cursor-pointer">
+              <span className="text-blue-500">+</span> Carica immagine
+            </label>
+            <input
+              id="image-upload"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageUpload}
             />
           </div>
+          {imageLoaded && (
+            <div className="text-green-500">Immagine caricata (120 caratteri)</div>
+          )}
         </div>
         <div className="flex justify-end">
           <button className="btn btn-primary" onClick={handlePublishClick}>
