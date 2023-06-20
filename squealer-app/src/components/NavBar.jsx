0bdfa803e15730af529eb83fd75a9bb2c722e04e
NartoTeroKK <marcoperozzi00@gmail.com>
2023-06-20 20:21:23 +0200

    prova Context

--- squealer-app/src/components/NavBar.jsx
+++ squealer-app/src/components/NavBar.jsx
@@ -1,42 +1,14 @@
 import { linkList } from "../router";
 import { Link, Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { useQuery } from "react-query";
-import { Const } from "../utils";
+
+import UserAvatar from "./UserAvatar";
 
 function NavBar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
-  const [leftoverChars, setLeftoverChars] = useState(null);
-  const [avatarPath, setAvatarPath] = useState(
-    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-  );
 
   function handleClick() {
     setIsNavExpanded(!isNavExpanded);
-  }
-
-  if (localStorage.getItem("userID") !== null) {
-    const userID = localStorage.getItem("userID").toString();
-
-    const fetchUser = async () => {
-      const res = await fetch(`${Const.apiurl}/user/${userID}`, {
-        headers: { Authorization: localStorage.token },
-      });
-
-      return await res.json();
-    };
-
-    const { data: user } = useQuery(["user", userID], fetchUser);
-
-    useEffect(() => {
-      if (user) {
-        if (user.propic_path !== "") {
-          setAvatarPath(user.propic_path);
-        }
-        setLeftoverChars(user.leftovers_chars);
-        console.log("User Leftover Chars:", user.leftovers_chars);
-      }
-    }, [user]);
   }
 
   return (
@@ -55,22 +27,17 @@ function NavBar() {
             </Link>
           </div>
 
+
+        <div
+          className={
+            isNavExpanded ? "grid md:flex md:mx-5" : "hidden md:flex md:mx-5"
+          }
+          >
+          {linkList}
+        </div>
+
           <div className="flex items-center">
-            <div className="md:hidden">
-              {localStorage.getItem("userID") !== null ? (
-                <Link to="/account">
-                  <div className="avatar">
-                    <div className="w-12 rounded-full ring ring-offset-base-100 ring-offset-0">
-                      <img src={avatarPath} />
-                    </div>
-                  </div>
-                </Link>
-              ) : (
-                <Link to="/login">
-                  <button className="btn btn-sm btn-outline">Sign in</button>
-                </Link>
-              )}
-            </div>
+            <UserAvatar />
 
             <button
               className="btn btn-square btn-ghost scale-125 mr-8 ml-4 md:hidden"
@@ -93,32 +60,6 @@ function NavBar() {
           </div>
         </div>
 
-        <div
-          className={
-            isNavExpanded ? "grid md:flex md:mx-5" : "hidden md:flex md:mx-5"
-          }
-        >
-          {linkList}
-        </div>
-        <div className="mr-8 hidden md:flex items-center">
-          {localStorage.getItem("userID") !== null ? (
-            <Link to="/account">
-              <div className="avatar">
-                <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-0">
-                  <img src={avatarPath} />
-                </div>
-              </div>
-              <div className="ml-2 font-bold">
-                D: {leftoverChars?.day || 0} W: {leftoverChars?.week || 0} M:{" "}
-                {leftoverChars?.month || 0}
-              </div>
-            </Link>
-          ) : (
-            <Link to="/login">
-              <button className="btn btn-outline">Sign in</button>
-            </Link>
-          )}
-        </div>
       </div>
       <Outlet />
     </div>
