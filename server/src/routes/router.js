edfe882b95b21d3b735f6898e897026ddda56dcf
NartoTeroKK <marcoperozzi00@gmail.com>
2023-06-21 18:27:00 +0200

    modifiche endpoint router ridondanti

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -36,13 +36,8 @@ appRouter.get(
 );
 
 appRouter.get("/api/users", userRoutes.getAllUsersFiltered);
-appRouter.patch("/api/user/:id", userRoutes.updateUser);
 appRouter.patch("/api/user/:id/chars", userRoutes.updateUserChars);
 appRouter.get("/api/user/:id/stats", userRoutes.getUserStats);
-appRouter.delete("/api/user/:id", userRoutes.deleteUser);
-appRouter.get("/api/user/manager/:id", userRoutes.getManager);
-
-appRouter.get("/api/users/managers", userRoutes.getAvailableManagers);
 appRouter.get(
   "/api/user/channels/all",
   verifyToken,
